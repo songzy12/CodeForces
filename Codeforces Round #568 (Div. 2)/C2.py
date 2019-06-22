0fc8306f628c8ea01957cc98c1ef3972b84690e8
songzy <songzy.thu@qq.com>
2019-06-22 09:16:38 +0800

    Sat, Jun 22, 2019  9:16:38 AM

---
+++ Codeforces Round #568 (Div. 2)/C2.py
@@ -0,0 +1,4 @@
+# http://codeforces.com/blog/entry/67829
+
+# 因为 T 的范围很小，于是我们可以记一个 count 数组
+# 然后每次检查 prefix 和超出了多少，并将 count 数组从大到小删减
---
+++ Codeforces Round #568 (Div. 2)/E.py
@@ -0,0 +1,3 @@
+# http://codeforces.com/blog/entry/67829
+# 因为 z 会覆盖 y, x
+# 所以从 z 开始从后往前检查
