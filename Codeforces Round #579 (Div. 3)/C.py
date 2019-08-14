2bad22e7d3c646c114b23c085fe38a63d3b1b71b
songzy <songzy.thu@qq.com>
2019-08-14 23:41:07 +0800

    Wed, Aug 14, 2019 11:41:07 PM

--- Codeforces Round #579 (Div. 3)/C.py
+++ Codeforces Round #579 (Div. 3)/C.py
@@ -9,19 +9,27 @@ def solve(a):
     for i in range(1, len(a)):
         res = math.gcd(res, a[i])
 
-    from collections import defaultdict
-    m = defaultdict(int)
-    d = 2
-    while res != 1:
-        while res % d == 0:
-            res = res / d
-            m[d] += 1
-        d += 1
+    cnt = 0
+    for k in range(1, int(res ** 0.5) + 1): # NOTE: sqrt matters
+        if res % k == 0:
+            cnt += 1
+            if res / k != k:
+                cnt += 1
+    return cnt
 
-    res = 1
-    for k in m:
-        res *= m[k] + 1
-    return res
+    # from collections import defaultdict
+    # m = defaultdict(int)
+    # d = 2
+    # while res != 1:
+    #     while res % d == 0:
+    #         res = res / d
+    #         m[d] += 1
+    #     d += 1
+
+    # res = 1
+    # for k in m:
+    #     res *= m[k] + 1
+    # return res
 
 
 print(solve(a))
---
+++ Codeforces Round #579 (Div. 3)/F1.py
@@ -0,0 +1,4 @@
+# https://codeforces.com/contest/1203/problem/F1
+# https://codeforces.com/blog/entry/69108
+
+# Then let's sort all projects in order of ai+bi in non-increasing order and go from left to right and take all of them
