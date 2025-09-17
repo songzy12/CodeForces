03727a1c6639952040b1620865301c6ec23b492a
Zhengyang Song <songzy_thu@163.com>
2025-09-17 21:40:47 +0800

    Problem B of Round 1049.

---
+++ Round/1049/B.py
@@ -0,0 +1,8 @@
+# https://codeforces.com/contest/2140/problem/B
+#
+# Given x, find y such that (x+y) | x#y
+
+T = int(input())
+for _ in range(T):
+    x = int(input())
+    print(2*x)
---
+++ Round/1049/README.md
@@ -0,0 +1,3 @@
+https://codeforces.com/contest/2140
+- Announcement: https://codeforces.com/blog/entry/146147
+- Editorial: https://codeforces.com/blog/entry/146218
