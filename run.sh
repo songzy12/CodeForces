1548d25b1f1346597a490eb00b1f89d3ac151600
Zhengyang Song <songzy_thu@163.com>
2026-08-13 12:33:00 +0800

    Add a run script to use the local solution checker.

---
+++ run.sh
@@ -0,0 +1,5 @@
+CODE_FILE=Round/2010/1/A.py
+INPUT_FILE=Round/2010/1/testdata/A.in
+EXPECTED_OUTPUT_FILE=Round/2010/1/testdata/A.out
+
+python3 check_solution.py $CODE_FILE $INPUT_FILE $EXPECTED_OUTPUT_FILE
